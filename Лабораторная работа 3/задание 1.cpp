c9645e93c6b032f8a13fcaf1a2697831d5786625
2xclown <arina.shedko0105@gmail.com>
2024-12-14 00:51:52 +0300

    Add files via upload

---
+++ Лабораторная работа 3/задание 1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+// Базовый класс "Деталь"
+class Detail {
+protected:
+    Detail() {} 
+public:
+    virtual ~Detail() = default; 
+    virtual void print() const = 0;
+};
+
+// Производный класс "Сборка"
+class Assembly : public Detail {
+protected:
+    Assembly() {} 
+public:
+    virtual ~Assembly() = default;
+    void print() const override {
+        cout << "Assembly object" << endl;
+    }
+};
+
+// Дружественная шаблонная функция для создания объектов
+template <typename T>
+shared_ptr<T> createObject() {
+    struct Creator : public T {
+        Creator() : T() {}
+    };
+    return make_shared<Creator>();
+}
+
+int main() {
+    // Хранилище объектов
+    vector<shared_ptr<Detail>> storage;
+
+    // Создание объектов через дружественную шаблонную функцию
+    storage.push_back(createObject<Assembly>());
+
+    // Вывод информации о каждом объекте
+    for (const auto& obj : storage) {
+        obj->print();
+    }
+
+    return 0;
+}
---
+++ Лабораторная работа 3/задание 2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Базовый класс
+class Base {
+public:
+    virtual void showBehavior() const {
+        cout << "Base behavior" << endl;
+    }
+    virtual ~Base() = default;
+};
+
+// Производный класс
+class Derived : public Base {
+public:
+    void showBehavior() const override {
+        cout << "Derived behavior" << endl;
+    }
+};
+
+// Функция для добавления объектов в хранилище
+void add(vector<unique_ptr<Base>>& storage, unique_ptr<Base> obj) {
+    storage.push_back(move(obj));
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    vector<unique_ptr<Base>> storage;
+
+    // Создание объектов
+    for (int i = 0; i < 10; ++i) {
+        if (rand() % 2 == 0) {
+            add(storage, make_unique<Base>());
+        }
+        else {
+            add(storage, make_unique<Derived>());
+        }
+    }
+
+    // Демонстрация поведения объектов
+    for (const auto& obj : storage) {
+        obj->showBehavior();
+    }
+
+    // Все ресурсы будут автоматически освобождены благодаря unique_ptr
+    return 0;
+}
