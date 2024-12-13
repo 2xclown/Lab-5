6e16179395b04150c49fadbdc47b6d0de07185f6
2xclown <arina.shedko0105@gmail.com>
2024-12-14 00:49:41 +0300

    Add files via upload

---
+++ Лабораторная работа 1/задание 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	system("chcp 1251");
+	const int size = 10;
+
+	double arr[size] = { 3.14, 2.71, 1.41, 1.73, 2.24, 3.33, 4.44, 5.55, 6.66, 7.77 };
+
+	cout << "Содержимое массива:\n";
+	for (int i = 0; i < size; ++i) {
+		cout << "arr[" << i << "] = " << arr[i] << endl;
+	}
+
+	cout << "\nАдрес начала массива: " << arr << endl;
+
+	cout << "\nРасстояние каждого элемента от начала массива (в байтах):\n";
+	for (int i = 0; i < size; ++i) {
+		cout << "Элемент arr[" << i << "] находится на расстоянии "
+			<< (reinterpret_cast<char*>(&arr[i]) - reinterpret_cast<char*>(arr)) << " байт.\n";
+	}
+
+	return 0;
+}
---
+++ Лабораторная работа 1/задание 2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+enum class BookType { Fiction, Technical };
+
+class Book {
+private:
+    string author;
+    string title;
+    BookType type;
+
+public:
+    Book(const string& author, const string& title, BookType type)
+        : author(author), title(title), type(type) {
+        if (author.empty() || title.empty()) {
+            throw invalid_argument("Имя автора и название книги не могут быть пустыми.");
+        }
+    }
+
+    BookType getType() const {
+        return type;
+    }
+};
+
+class Library {
+private:
+    vector<Book*> books;
+
+public:
+    void addBook(const string& author, const string& title, BookType type) {
+        books.push_back(new Book(author, title, type));
+    }
+
+    void printBookCounts() const {
+        int fictionCount = 0;
+        int technicalCount = 0;
+
+        for (const auto& book : books) {
+            switch (book->getType()) {
+            case BookType::Fiction:
+                fictionCount++;
+                break;
+            case BookType::Technical:
+                technicalCount++;
+                break;
+            }
+        }
+
+        cout << "Количество художественных книг: " << fictionCount << endl;
+        cout << "Количество технических книг: " << technicalCount << endl;
+    }
+
+    ~Library() {
+        for (auto& book : books) {
+            delete book;
+        }
+    }
+};
+
+int main() {
+    system("chcp 1251");
+    try {
+        Library library;
+
+        library.addBook("Лев Толстой", "Война и мир", BookType::Fiction);
+        library.addBook("Джордж Оруэлл", "1984", BookType::Fiction);
+        library.addBook("Брайан Керниган", "Язык программирования C", BookType::Technical);
+        library.addBook("Герхардт Вайнберг", "История Второй мировой войны", BookType::Technical);
+
+        library.printBookCounts();
+    }
+    catch (const exception& e) {
+        cerr << "Ошибка: " << e.what() << endl;
+    }
+
+    return 0;
+}
